ab90e403ae70a01fd5b820ad69d59c9bf0eb7cd7
Adhin prakash <adhinprakash70@gmail.com>
2023-07-30 21:07:05 +0530

    ngo accesstoken stored

--- lib/controller/provider/ngo_login.dart
+++ lib/controller/provider/ngo_login.dart
@@ -1,31 +1,28 @@
-
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:give_gobble/controller/api/api_url.dart';
-import 'package:http/http.dart'as http;
+import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
+
 class NgoLogin extends ChangeNotifier {
   final emailOResnamecontroller = TextEditingController();
   final passwordcontroller = TextEditingController();
 
-
- bool _isLoading = false;
+  bool _isLoading = false;
 
   bool get isLoading => _isLoading;
 
   void _setIsLoading(bool value) {
     _isLoading = value;
     notifyListeners();
-  } 
-
-
+  }
 
   Future<int> ngologin(context) async {
     final String emailorResname = emailOResnamecontroller.text;
     final String password = passwordcontroller.text;
     try {
-            _setIsLoading(true); // Show the loader
+      _setIsLoading(true); // Show the loader
 
       final response = await http.post(
         Uri.parse(Api.ngologin),
@@ -33,59 +30,52 @@ class NgoLogin extends ChangeNotifier {
       );
       _setIsLoading(false); // Show the loader
 // print(response.body);
-      if (response.statusCode ==  401) {
-       
-       
-      
-
+      if (response.statusCode == 401) {
         emailOResnamecontroller.clear();
         passwordcontroller.clear();
 
-  notifyListeners();
+        notifyListeners();
         return 401;
-      } else if(response.statusCode ==  403){
+      } else if (response.statusCode == 403) {
         notifyListeners();
         return 403;
-        // Handle other status codes (e.g., 500 for server error, etc.).
-    
       } else {
+        SharedPreferences sharedPreferences =
+            await SharedPreferences.getInstance();
+        sharedPreferences.setString(
+            'ngoRefresh', jsonDecode(response.body)['refreshToken']);
+
+        sharedPreferences.setString(
+            'ngoAccess', jsonDecode(response.body)['accessToken']);
+
         notifyListeners();
         return 201;
-        // Handle other status codes (e.g., 500 for server error, etc.).
-    
       }
     } catch (error) {
-      // Handle error while making the HTTP request
-    print(error);
+      print(error);
       notifyListeners();
       return 500;
     }
-  
   }
 
-
-
   Future<bool> ngologout(BuildContext context) async {
-    // Clear user login state and user information from shared preferences.
-    try{
-    _setIsLoading(true);
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.remove('isNgoRegistered');
-    prefs.remove('isNgoLoggedIn');
-    prefs.remove('userId');
-    prefs.remove('userRole');
-    prefs.remove('accessToken');
-    prefs.remove('refreshToken');
-    _setIsLoading(false);
-    notifyListeners();
-return true;
-    }
-    catch(e){
+    try {
+      _setIsLoading(true);
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      prefs.remove('isNgoRegistered');
+      prefs.remove('isNgoLoggedIn');
+      prefs.remove('userId');
+      prefs.remove('userRole');
+      prefs.remove('accessToken');
+      prefs.remove('refreshToken');
+      _setIsLoading(false);
       notifyListeners();
-return false;
+      return true;
+    } catch (e) {
+      notifyListeners();
+      return false;
     }
     // Navigate to the login page after logout.
     // ignore: use_build_context_synchronously
-
   }
 }
--- lib/controller/provider/ngo_signup.dart
+++ lib/controller/provider/ngo_signup.dart
@@ -3,121 +3,119 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:give_gobble/controller/api/api_url.dart';
-import 'package:http/http.dart'as http;
+import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
+
 class NgoProvider extends ChangeNotifier {
   final ngonamecontroller = TextEditingController();
   final emailcontroller = TextEditingController();
   final locationcontroller = TextEditingController();
-  final ngotypecontroller=TextEditingController();
-  final pincodecontroller=TextEditingController();
+  final ngotypecontroller = TextEditingController();
+  final pincodecontroller = TextEditingController();
   final passwordcontroller = TextEditingController();
   final otpcontroller = TextEditingController();
   String uId = "";
   String errormsg = '';
   String otperror = '';
-  
- bool _isLoading = false;
+  String emaildetail = '';
+
+  bool _isLoading = false;
 
   bool get isLoading => _isLoading;
 
   void _setIsLoading(bool value) {
     _isLoading = value;
     notifyListeners();
-  } 
+  }
 
-
-
- bool _isotpLoading = false;
+  bool _isotpLoading = false;
 
   bool get isotpLoading => _isotpLoading;
 
   void _otpLoading(bool value) {
     _isotpLoading = value;
     notifyListeners();
-  } 
-
+  }
 
   Future<bool> ngoregister() async {
     final String username = ngonamecontroller.text;
     final String email = emailcontroller.text;
     final String address = locationcontroller.text;
     final String password = passwordcontroller.text;
-    final String ngotype=ngotypecontroller.text;
-    final String pincode=pincodecontroller.text;
+    final String ngotype = ngotypecontroller.text;
+    final String pincode = pincodecontroller.text;
     try {
       final response = await http.post(Uri.parse(Api.ngosignUpstepone), body: {
         "email": email,
         "username": username,
         "password": password,
         "address": address,
-        "ngoType":ngotype,
-        "pincode":pincode
+        "ngoType": ngotype,
+        "pincode": pincode
       });
-      final Map<String, dynamic> data = json.decode(response.body);
+      final Map<String, dynamic> data  = json.decode(response.body);
       uId = data["uId"];
       log(response.body);
-  String errorMessage = data["message"] ?? "Unknown error occurred";
+      // String errorMessage = data["message"] ?? "Unknown error occurred";
       if (response.statusCode == 200) {
-
-
         notifyListeners();
         return true;
-      }else{
-        
-        notifyListeners();
-        return false;
-
-      }
-    } catch (e) {
-      notifyListeners();
-      return false;    
-    }
-
-  }
-
-  Future<bool> ngoregistertwo(context) async {
-    final String username = ngotypecontroller.text;
-    final String email = emailcontroller.text;
-    final String address = locationcontroller.text;
-    final String password = passwordcontroller.text;
-       final String ngotype=ngotypecontroller.text;
-    final String pincode=pincodecontroller.text;
-    final String otp = otpcontroller.text;
-    try {
-      print(uId);
-      print(otpcontroller);
-      final response =
-          await http.post(Uri.parse(Api.ngosignUpsteptwo + uId), body: {
-       "email": email,
-        "username": username,
-        "password": password,
-        "address": address,
-        "ngoType":ngotype,
-        "pincode":pincode,
-        "enteredOtp": otp
-      });
-      log(response.statusCode.toString());
-      log(response.body);
-
-      if (response.statusCode == 201) {
-       
-  // SharedPreferences prefs = await SharedPreferences.getInstance();
-  //     prefs.setBool('isNgoRegistered', true);
-
-
-notifyListeners();
-return true;
-
-      }else{
+      } else {
         notifyListeners();
         return false;
       }
     } catch (e) {
       notifyListeners();
       return false;
-
     }
+  }
 
+  Future<int> ngoregistertwo(context) async {
+    final String username = ngotypecontroller.text;
+    final String email = emailcontroller.text;
+    final String address = locationcontroller.text;
+    final String password = passwordcontroller.text;
+    final String ngotype = ngotypecontroller.text;
+    final String pincode = pincodecontroller.text;
+    final String otp = otpcontroller.text;
+    try {
+      print(uId);
+      print(otpcontroller);
+      final response =
+          await http.post(Uri.parse(Api.ngosignUpsteptwo + uId), body: {
+        "email": email,
+        "username": username,
+        "password": password,
+        "address": address,
+        "ngoType": ngotype,
+        "pincode": pincode,
+        "enteredOtp": otp
+      });
+      log(response.statusCode.toString());
+      log(response.body);
+
+      if (response.statusCode == 400) {
+      
+
+        // SharedPreferences prefs = await SharedPreferences.getInstance();
+        //     prefs.setBool('isNgoRegistered', true);
+
+        notifyListeners();
+        return 400;
+      } else {
+        notifyListeners();
+        //   SharedPreferences sharedPreferences =
+        //     await SharedPreferences.getInstance();
+        // sharedPreferences.setString(
+        //     'ngoRefresh', jsonDecode(response.body)['refreshToken']);
+
+        // sharedPreferences.setString(
+        //     'ngoAccess', jsonDecode(response.body)['accessToken']);
+        return 201;
+      }
+    } catch (e) {
+      notifyListeners();
+      return 500;
+    }
   }
 }
