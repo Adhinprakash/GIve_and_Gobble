dbec64d6d6e238188f88e85db92697c5d5fc0fa9
Adhin prakash <adhinprakash70@gmail.com>
2023-08-05 15:25:29 +0530

    newapi function created

---
+++ lib/controller/provider/news_request/news_request.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:give_gobble/controller/api/api_url.dart';
+import 'package:give_gobble/model/news_model/article/article.dart';
+import 'package:http/http.dart'as http;
+
+class NewsApi extends ChangeNotifier{
+bool isLoading=true;
+List<Article>articles=[];
+  Future<List<Article>>getnews()async{
+    isLoading=true;
+    final response=await http.get(Uri.parse(Api.endpointurl));
+    print(response.body);
+        if(response.statusCode==200){
+    
+      Map<String,dynamic> json=jsonDecode(response.body);
+      List<dynamic>body=json['articles'];
+
+      articles=body.map((dynamic item) => Article.fromJson(item)).toList();
+            isLoading=false;
+
+      notifyListeners();
+      isLoading=false;
+      return articles;  
+    }else{
+      throw ("cant get the Article");
+    }
+  }
+}
