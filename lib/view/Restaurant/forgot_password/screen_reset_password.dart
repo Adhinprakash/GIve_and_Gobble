1d7efd48139b7e1fc0f58ecc6536aec869c488f9
Adhin prakash <adhinprakash70@gmail.com>
2023-09-03 12:07:45 +0530

    forgot password reset password page error fixed

--- lib/view/Restaurant/forgot_password/screen_reset_password.dart
+++ lib/view/Restaurant/forgot_password/screen_reset_password.dart
@@ -22,142 +22,146 @@ class ScreenResetpassword extends StatelessWidget {
           ),
           child: Form(
             key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
+            child: ListView(
               children: [
-                Text(
-                  "Create new Password",
-                  style: textStyleFuc(
-                      weight: FontWeight.bold, color: kBlack, size: 24),
-                ),
-                Lottie.asset('assets/animation/animation_lkzmtbk4.json',
-                    height: 300, width: 300),
-                Center(
-                  child: Text(
-                    "Your New Password Must be different\n from previously used password ",
-                    style: textStyleFuc(
-                        weight: FontWeight.w500, color: kBlack, size: 17),
-                  ),
-                ),
-                kheight30,
-                kheight30,
-                SizedBox(
-                  height: 8.h,
-                  width: 80.w,
-                  child: TextFormField(
-                    controller:
-                        Provider.of<ResForgotpassword>(context, listen: false)
-                            .resemailcontroller,
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Please enter your Email';
-                      }
-                      return null;
-                    },
-                    cursorColor: Colors.black,
-                    style: const TextStyle(),
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12)),
-                      labelText: 'Enter your email',
-                      fillColor: Colors.amber,
-                      labelStyle: const TextStyle(color: Colors.black),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                        borderSide: const BorderSide(color: Colors.black),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Create new Password",
+                      style: textStyleFuc(
+                          weight: FontWeight.bold, color: kBlack, size: 24),
+                    ),
+                    Lottie.asset('assets/animation/animation_lkzmtbk4.json',
+                        height: 300, width: 300),
+                    Center(
+                      child: Text(
+                        "Your New Password Must be different\n from previously used password ",
+                        style: textStyleFuc(
+                            weight: FontWeight.w500, color: kBlack, size: 17),
                       ),
                     ),
-                  ),
-                ),
-                SizedBox(
-                  height: 8.h,
-                  width: 80.w,
-                  child: TextFormField(
-                    controller:
-                        Provider.of<ResForgotpassword>(context, listen: false)
-                            .newpasswordcontroller,
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Please enter your Email';
-                      }
-                      return null;
-                    },
-                    cursorColor: Colors.black,
-                    style: const TextStyle(),
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12)),
-                      labelText: 'Enter new password',
-                      fillColor: Colors.amber,
-                      labelStyle: const TextStyle(color: Colors.black),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                        borderSide: const BorderSide(color: Colors.black),
-                      ),
-                    ),
-                  ),
-                ),
-                kheight30,
-                kheight30,
-                Container(
-                    decoration:
-                        BoxDecoration(borderRadius: BorderRadius.circular(2)),
-                    width: 350,
-                    height: 50,
-                    child: ElevatedButton(
-                      onPressed: () async {
-                        if (_formKey.currentState!.validate()) {
-                          int isverify = await Provider.of<ResForgotpassword>(
-                                  context,
-                                  listen: false)
-                              .savePassword();
-
-                          if (isverify == 201) {
-                            // ignore: use_build_context_synchronously
-                            Navigator.pushAndRemoveUntil(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => ScreenRestaurantLogin(),
-                              ),
-                              (route) => false,
-                            );
-                          } else if (isverify == 401) {
-                            // ignore: use_build_context_synchronously
-                            showDialog(
-                              context: context,
-                              builder: (context) => AlertDialog(
-                                title: const Text('Error'),
-                                content: const Text('Enter a valid otp'),
-                                actions: [
-                                  TextButton(
-                                    onPressed: () => Navigator.pop(context),
-                                    child: const Text('OK'),
-                                  ),
-                                ],
-                              ),
-                            );
-                          } else {
-                            // ignore: use_build_context_synchronously
-                            showDialog(
-                              context: context,
-                              builder: (context) => AlertDialog(
-                                title: const Text('Oops!'),
-                                content: const Text('Internal server error'),
-                                actions: [
-                                  TextButton(
-                                    onPressed: () => Navigator.pop(context),
-                                    child: const Text('OK'),
-                                  ),
-                                ],
-                              ),
-                            );
+                    kheight30,
+                    kheight30,
+                    SizedBox(
+                      height: 8.h,
+                      width: 80.w,
+                      child: TextFormField(
+                        controller:
+                            Provider.of<ResForgotpassword>(context, listen: false)
+                                .resemailcontroller,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Please enter your Email';
                           }
-                        }
-                      },
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: kpink, foregroundColor: kwhite),
-                      child: const Text("Save"),
-                    ))
+                          return null;
+                        },
+                        cursorColor: Colors.black,
+                        style: const TextStyle(),
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12)),
+                          labelText: 'Enter your email',
+                          fillColor: Colors.amber,
+                          labelStyle: const TextStyle(color: Colors.black),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: const BorderSide(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8.h,
+                      width: 80.w,
+                      child: TextFormField(
+                        controller:
+                            Provider.of<ResForgotpassword>(context, listen: false)
+                                .newpasswordcontroller,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Please enter your Email';
+                          }
+                          return null;
+                        },
+                        cursorColor: Colors.black,
+                        style: const TextStyle(),
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12)),
+                          labelText: 'Enter new password',
+                          fillColor: Colors.amber,
+                          labelStyle: const TextStyle(color: Colors.black),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: const BorderSide(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ),
+                    kheight30,
+                    kheight30,
+                    Container(
+                        decoration:
+                            BoxDecoration(borderRadius: BorderRadius.circular(2)),
+                        width: 350,
+                        height: 50,
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            if (_formKey.currentState!.validate()) {
+                              int isverify = await Provider.of<ResForgotpassword>(
+                                      context,
+                                      listen: false)
+                                  .savePassword();
+
+                              if (isverify == 201) {
+                                // ignore: use_build_context_synchronously
+                                Navigator.pushAndRemoveUntil(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => ScreenRestaurantLogin(),
+                                  ),
+                                  (route) => false,
+                                );
+                              } else if (isverify == 401) {
+                                // ignore: use_build_context_synchronously
+                                showDialog(
+                                  context: context,
+                                  builder: (context) => AlertDialog(
+                                    title: const Text('Error'),
+                                    content: const Text('Enter a valid otp'),
+                                    actions: [
+                                      TextButton(
+                                        onPressed: () => Navigator.pop(context),
+                                        child: const Text('OK'),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              } else {
+                                // ignore: use_build_context_synchronously
+                                showDialog(
+                                  context: context,
+                                  builder: (context) => AlertDialog(
+                                    title: const Text('Oops!'),
+                                    content: const Text('Internal server error'),
+                                    actions: [
+                                      TextButton(
+                                        onPressed: () => Navigator.pop(context),
+                                        child: const Text('OK'),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              }
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: kpink, foregroundColor: kwhite),
+                          child: const Text("Save"),
+                        ))
+                  ],
+                ),
               ],
             ),
           ),
